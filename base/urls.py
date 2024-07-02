b11fdc9fdf16910507b19f5f229b0569771412f5
Maks <fray09@inbox.ru>
2024-07-02 19:28:22 +0600

    Add task update view to app base

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', views.TaskDetail.as_view(), name='task'),
-    path('task-create', views.TaskCreate.as_view(), name='task-create')
+    path('task-create', views.TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', views.TaskUpdate.as_view(), name='task-update'),
 ]
